dc8465809d650d5ebb8f796b10742e523077b87b
Jérôme Koch <jerome.koch@gmx.ch>
2019-05-29 17:32:19 +0200

    #2 - Das erste Tile hinzugefügt - Funktion erstellt, ob das Tile leer oder besetzt ist

--- src/captain/Main.java
+++ src/captain/Main.java
@@ -1,10 +1,20 @@
 package captain;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    //Returns true if a tile is occupied
+    //Returns false if a tile is empty
+    public static boolean tileOccupied(ArrayList<Player> tile){
+        if (tile.size() > 0) return true;
+        else return false;
+    }
 
     public static void main(String[] args) {
 
         Player Nils = new Player();
+        Player Tom = new Player();
 
         Nils.setName("Gronak - Sleeper of Men");
         Nils.setMaxHealth(30);
@@ -17,5 +27,12 @@ public class Main {
         Nils.gainHealth(15);
         Nils.loseHealth(33);
         Nils.gainHealth(2);
+
+        //first tile
+        ArrayList<Player> tile1 = new ArrayList<Player>();
+            tile1.add(Nils);
+            tile1.add(Tom);
+
+        if (tileOccupied(tile1)) System.out.println("Tile 1 is occupied");
     }
 }
