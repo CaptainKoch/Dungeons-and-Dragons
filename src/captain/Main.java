97f41cdf56fa778d21c41133d6c05c362bf00fcf
Jérôme Koch <jerome.koch@gmx.ch>
2019-05-31 14:41:32 +0200

    #2
    Erstellen einer Tile Klasse mit ArrayList<Player> objekt.
    Funktioniert noch nicht wie gewollt.

--- src/captain/Main.java
+++ src/captain/Main.java
@@ -1,22 +1,20 @@
 package captain;
 
-import java.util.ArrayList;
-
 public class Main {
 
-    //Returns true if a tile is occupied
-    //Returns false if a tile is empty
-    public static boolean tileOccupied(ArrayList<Player> tile){
-        if (tile.size() > 0) return true;
-        else return false;
-    }
 
     public static void main(String[] args) {
 
         Player Nils = new Player();
         Player Tom = new Player();
 
-        Nils.setName("Gronak - Sleeper of Men");
+        Tile tile1 = new Tile();
+
+        tile1.addPlayer(Nils);
+
+        if (tile1.isOccupied()) System.out.println("Tile is occupied");
+
+        /*Nils.setName("Gronak - Sleeper of Men");
         Nils.setMaxHealth(30);
         Nils.setTempHealth(15);
 
@@ -27,12 +25,6 @@ public class Main {
         Nils.gainHealth(15);
         Nils.loseHealth(33);
         Nils.gainHealth(2);
-
-        //first tile
-        ArrayList<Player> tile1 = new ArrayList<Player>();
-            tile1.add(Nils);
-            tile1.add(Tom);
-
-        if (tileOccupied(tile1)) System.out.println("Tile 1 is occupied");
+        */
     }
 }
---
+++ src/captain/Tile.java
@@ -0,0 +1,21 @@
+package captain;
+
+import java.util.ArrayList;
+
+public class Tile {
+    private ArrayList<Player> players;
+
+    public boolean isOccupied() {
+        if (players.size() > 0) return true;
+        else return false;
+    }
+
+    public void addPlayer(Player newPlayer) {
+        players.add(newPlayer);
+    }
+
+    public void removePlayer(Player playerToRemove) {
+        players.remove(playerToRemove);
+    }
+
+}
